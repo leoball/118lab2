7ce83d22a05ad56f5de4c62aa1065b10529d6dd6
Andrew Xue <andyzhouyangxue@g.ucla.edu>
2018-03-14 21:17:05 -0700

    check_time_out client

--- client.c
+++ client.c
@@ -101,10 +101,11 @@ int getPacket(int sockfd, char* message, size_t* len, struct sockaddr *src_addr,
     return result;
 }
 
-int check_time_out(int sock_fd, struct timeval tv){
+int check_time_out(int sock_fd){
     fd_set read_fds;
     FD_ZERO(&read_fds);
     FD_SET(sock_fd, &read_fds);
+    struct timeval tv;
     tv.tv_sec = 0;
     tv.tv_usec = RETRANS_TIME * 1000;
     int i = 0;
@@ -170,7 +171,7 @@ int main(int argc, char* argv[]){
     clock_gettime(CLOCK_MONOTONIC_RAW, &begin);
     //do the syn
     //fd_set read_fds;
-    struct timeval tv;
+    /*struct timeval tv;*/
     //we set ret equals 2 when successfully receive message from syn
     while(ret != 2){
         //send syn packet 
@@ -194,7 +195,7 @@ int main(int argc, char* argv[]){
             fprintf(stderr, "packet timeout.\n");
             ret = 1;
         }*/
-        if (check_time_out(sock_fd, tv)){
+        if (check_time_out(sock_fd)){
             fprintf(stderr, "packet timeout.\n");
             ret = 1;
         }
@@ -236,13 +237,17 @@ int main(int argc, char* argv[]){
             fprintf(stdout, "Sending packet 0 Retransmission\n");
         ret = 2;
 
-        fd_set read_fds;
+/*        fd_set read_fds;
         FD_ZERO(&read_fds);
         FD_SET(sock_fd, &read_fds);
         tv.tv_sec = 0;
         tv.tv_usec = RETRANS_TIME * 1000;
         int i = 0;
         if((i = select(sock_fd + 1, &read_fds, NULL, NULL, &tv)) == 0){
+            fprintf(stderr, "packet timeout.\n");
+            ret = 1;
+        }*/
+        if (check_time_out(sock_fd)){
             fprintf(stderr, "packet timeout.\n");
             ret = 1;
         }
